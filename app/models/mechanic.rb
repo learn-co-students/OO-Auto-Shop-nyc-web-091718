6396ee0e279d9a33a2536f5cc591d31067388a11
DDarrow123 <ddarrowphl@gmail.com>
2018-09-30 15:48:11 -0400

    built auto shop lab

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,42 @@
+require 'pry'
+
 class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
+
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def car_service
+    Car.all.select do |car|
+      car.mechanic == self
+      # binding.pry
+    end
+  end
+
+  #Get a list of all the car owners that go to a specific mechanic
+  def mechanic_customers
+    self.car_service.select do |mechanic|
+      mechanic.car_owner
+    end
+  end
+
+  #Get a list of the names of all car owners who go to a specific mechanic
+  def owner_names
+    self.car_service.map do |mechanic|
+      mechanic.car_owner.name
+    end
+  end
+
 
 
 end
